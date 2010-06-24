42b11a55a0597c012044ab22f2e4b992ebcfe122
davidmandle <davidmandle@a5c9a0f3-a0d0-8808-10fc-6cde0bab7d8d>
2010-06-24 07:26:15 +0000

    Modied the action server to "lead" the teleop controller to the set point rather than giving a single setpoint at the final position

--- chessbot/cart_interp/cartesian_arm_server.py
+++ chessbot/cart_interp/cartesian_arm_server.py
@@ -13,6 +13,7 @@ from geometry_msgs.msg import *
 class CartesianArmServer:
     def __init__(self, name):
         self.success_threshold = rospy.get_param('success_threshold')
+        self.max_lead = rospy.get_param('max_lead')
         self.targetpose = None
         self.currentpose = None
         self.linear_position_error = None
@@ -24,12 +25,12 @@ class CartesianArmServer:
     def callback(self, data):
         self.currentpose = data.pose
         if (self.targetpose is not None):
-            self.linear_position_error=math.sqrt((self.targetpose.position.x-data.pose.position.x) ** 2 + (self.targetpose.position.y-data.pose.position.y) ** 2 + (self.targetpose.position.z-data.pose.position.z) ** 2)
+            self.linear_position_error=euclidean_distance(self.targetpose.position, data.pose.position)
             rospy.logdebug(str(self.linear_position_error))
     
     def execute(self, goal):
         success = True
-        rate = rospy.Rate(1.0)
+        rate = rospy.Rate(10.0)
         self.targetpose = goal.setpoint.pose
         self.linear_position_error = None
         while (self.linear_position_error is None or self.linear_position_error > self.success_threshold) and not rospy.is_shutdown():
@@ -37,7 +38,15 @@ class CartesianArmServer:
                 self._as.set_preempted()
                 success = False
                 break
-            self.pub_posestamped.publish(goal.setpoint)
+            setpoint = goal.setpoint
+            goal_distance = euclidean_distance(currentpose, targetpose)
+            if goal_distance > max_lead:
+                scaling_factor = max_lead/goal_distance
+                setpoint.point.x = (setpoint.point.x - currentpose.position.x)*scaling_factor+currentpose.position.x
+                setpoint.point.y = (setpoint.point.y - currentpose.position.y)*scaling_factor+currentpose.position.y
+                setpoint.point.z = (setpoint.point.z - currentpose.position.z)*scaling_factor+currentpose.position.z
+                
+            self.pub_posestamped.publish(setpoint)
             feedback = cart_interp.msg.CartesianArmServerFeedback()
             feedback.currentpoint = self.currentpose
             rate.sleep()
@@ -45,7 +54,10 @@ class CartesianArmServer:
             result = cart_interp.msg.CartesianArmServerResult()
             result.endpoint = self.currentpose
             self._as.set_succeeded(result)
-        
+
+def euclidean_distance(point1, point2):
+    return math.sqrt((point1.x-point2.x) ** 2 + (point1.y-point2.y) ** 2 + (point1.z-point2.z) ** 2)
+
 def main():
     rospy.init_node('cartesian_arm_server')
     server = CartesianArmServer(rospy.get_name())
