680a77d899e1cf50f810ba5f1eac37320cc1d246
davidmandle <davidmandle@a5c9a0f3-a0d0-8808-10fc-6cde0bab7d8d>
2010-06-25 00:56:33 +0000

    Some fixes for two-arm chessbot

--- trunk/chessbot/chessbot/twoarmchessmaster.py
+++ trunk/chessbot/chessbot/twoarmchessmaster.py
@@ -8,7 +8,7 @@ def main():
     rospy.init_node('chessmaster')
     right_chessbot_client = actionlib.SimpleActionClient('right_chessbot/chessbot', chessbot.msg.ChessbotAction)
     right_chessbot_client.wait_for_server()
-    left_chessbot_client = actionlib.SimpleActionClient('right_chessbot/chessbot', chessbot.msg.ChessbotAction)
+    left_chessbot_client = actionlib.SimpleActionClient('left_chessbot/chessbot', chessbot.msg.ChessbotAction)
     left_chessbot_client.wait_for_server()
     move = chessbot.msg.ChessbotGoal(0, 0, 7, 7, False)
     right_chessbot_client.send_goal(move)
