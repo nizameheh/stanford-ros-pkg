0a7e116eff463c57bbe803af130e83da3738e0b1
morganquigley <morganquigley@a5c9a0f3-a0d0-8808-10fc-6cde0bab7d8d>
2010-07-08 01:33:59 +0000

    first two joints are looking good as far as stepper-counting and de-coupling go.

--- openarms/state/oa-state.cpp
+++ openarms/state/oa-state.cpp
@@ -4,27 +4,42 @@
 #include <tf/transform_broadcaster.h>
 #include "openarms/ArmSensors.h"
 
-const uint32_t NUM_JOINTS = 2;
-static double g_joint_bias[2];
-static double g_joint_pos[2];
+static double g_joint_pos[7];
 ros::Publisher *g_joint_pub = NULL;
+static int32_t g_stepper_offsets[4], g_servo_offsets[3];
+bool g_offset_init_complete = false;
 
 void sensors_cb(const openarms::ArmSensors::ConstPtr &sensors)
 {
+  if (!g_offset_init_complete)
+  {
+    g_offset_init_complete = true;
+    for (int i = 0; i < 4; i++)
+      g_stepper_offsets[i] = sensors->pos[i];
+  }
+
   // 10-microstep, 1.8 deg/step
-  g_joint_pos[0] = g_joint_bias[0] - sensors->pos[0] / 2.0 / 10.0 * 1.8 * 3.1415 / 180.0 / 6.0; 
+  g_joint_pos[0] = -(sensors->pos[0] - g_stepper_offsets[0]) / 2.0 / 10.0 * 1.8 * 3.1415 / 180.0 / 6.5; 
+  g_joint_pos[1] = (sensors->pos[1] - g_stepper_offsets[1]) / 2.0 / 10.0 * 1.8 * 3.1415 / 180.0 / 10.133 + g_joint_pos[0] / 1.97; 
+  /*
   static int print_count = 0;
   if (print_count++ % 50 == 0)
     ROS_INFO("%10d %10f", sensors->pos[0], g_joint_pos[0]);
+  */
   if (g_joint_pub)
   {
     sensor_msgs::JointState joint_state;
-    joint_state.name.resize(2);
-    joint_state.position.resize(2);
-    joint_state.name[0] = "shoulderpitch_joint";
-    joint_state.position[0] = g_joint_pos[0];
-    joint_state.name[1] = "shoulderlift_joint";
-    joint_state.position[1] = 0;
+    joint_state.name.resize(7);
+    joint_state.position.resize(7);
+    joint_state.name[0] = "shoulder1";
+    joint_state.name[1] = "shoulder2";
+    joint_state.name[2] = "shoulder3";
+    joint_state.name[3] = "elbow";
+    joint_state.name[4] = "wrist1";
+    joint_state.name[5] = "wrist2";
+    joint_state.name[6] = "wrist3";
+    for (int i = 0; i < 7; i++)
+      joint_state.position[i] = g_joint_pos[i];
     joint_state.header.stamp = ros::Time::now();
     g_joint_pub->publish(joint_state);
   }
@@ -34,11 +49,12 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "oa_state");
   ros::NodeHandle n;
-  ros::NodeHandle n_private("~");
-  for (uint32_t i = 0; i < NUM_JOINTS; i++)
-    g_joint_bias[i] = 0;
-  n_private.getParam("joint0bias", g_joint_bias[0]);
-  n_private.getParam("joint1bias", g_joint_bias[1]);
+  for (uint32_t i = 0; i < 7; i++)
+    g_joint_pos[i] = 0;
+  for (uint32_t i = 0; i < 4; i++)
+    g_stepper_offsets[i] = 0;
+  for (uint32_t i = 0; i < 3; i++)
+    g_servo_offsets[i] = 0;
   ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
   g_joint_pub = &joint_pub; // ugly ugly
   ros::Subscriber sensor_sub = n.subscribe("arm_sensors", 1, sensors_cb);
@@ -46,7 +62,7 @@ int main(int argc, char **argv)
   ros::Rate loop_rate(100);
   geometry_msgs::TransformStamped world_trans;
   world_trans.header.frame_id = "world";
-  world_trans.child_frame_id = "torso";
+  world_trans.child_frame_id = "torso_link";
   world_trans.transform.translation.x = 0;
   world_trans.transform.translation.y = 0;
   world_trans.transform.translation.z = 0;
