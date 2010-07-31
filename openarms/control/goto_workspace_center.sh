6612f9c572ad8c4f95bbdb8ad050821467a1c1a9
morganquigley <morganquigley@a5c9a0f3-a0d0-8808-10fc-6cde0bab7d8d>
2010-07-31 22:05:16 +0000

    controller and simulator work now. oa9 model appears correct.

--- openarms/control/goto_workspace_center.sh
+++ openarms/control/goto_workspace_center.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-`rospack find openarms`/control/set_joint_target.py 0.3 -0.3 -1.0 0 0 0 0 0
+`rospack find openarms`/control/set_joint_target.py -0.7 -2.5 -0.3 0.7 1.7 0.7 0 0
--- openarms/sim/armbus_sim.cpp
+++ openarms/sim/armbus_sim.cpp
@@ -7,7 +7,7 @@
 
 openarms::ArmSensors sensors_msg;
 
-int32_t g_stepper_vel[4], g_servo_torque[3];
+int32_t g_stepper_vel[4], g_servo_torque[4];
 int g_stepper_dirs[4] = {1, -1, 1, 1};
 
 void actuators_cb(const openarms::ArmActuators::ConstPtr &msg)
@@ -17,14 +17,14 @@ void actuators_cb(const openarms::ArmActuators::ConstPtr &msg)
     ROS_INFO("woah I was expecting 4 stepper velocities");
     return;
   }
-  if (msg->servo_torque.size() != 3)
+  if (msg->servo_torque.size() != 4)
   {
     ROS_INFO("ahhh I was expecting 3 servo torques");
     return;
   }
   for (int i = 0; i < 4; i++)
     g_stepper_vel[i] = g_stepper_dirs[i] * msg->stepper_vel[i];
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < 4; i++)
     g_servo_torque[i] = msg->servo_torque[i];
 }
 
@@ -36,15 +36,15 @@ int main(int argc, char **argv)
   ros::Publisher pub = n.advertise<openarms::ArmSensors>("arm_sensors",1);
   ros::Time t_prev = ros::Time::now();
 
-  double stepper_pos[4], servo_pos[3];
+  double stepper_pos[4], servo_pos[4];
 
   for (int i = 0; i < 4; i++)
     stepper_pos[i] = g_stepper_vel[i] = 0;
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < 4; i++)
     servo_pos[i] = g_servo_torque[i] = 0;
 
   openarms::ArmSensors sensors_msg;
-  sensors_msg.pos.resize(7);
+  sensors_msg.pos.resize(8);
 
   while (n.ok())
   {
@@ -62,10 +62,10 @@ int main(int argc, char **argv)
         stepper_pos[i] += d * g_stepper_dirs[i] * g_stepper_vel[i];      
         sensors_msg.pos[i] = (int32_t)stepper_pos[i];
       }
-      for (int i = 0; i < 3; i++)
+      for (int i = 0; i < 4; i++)
       {
-        servo_pos[i] += d * 0.1 * g_servo_torque[i];
-        sensors_msg.pos[i+4] = (int32_t)g_servo_torque[i];
+        servo_pos[i] += d * 1 * g_servo_torque[i];
+        sensors_msg.pos[i+4] = (int32_t)servo_pos[i];
       }
       pub.publish(sensors_msg);
     }
