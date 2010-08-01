aa159802137a23aa8f06651f412447ec771ece17
morganquigley <morganquigley@a5c9a0f3-a0d0-8808-10fc-6cde0bab7d8d>
2010-08-01 03:25:08 +0000

    more tweaks for oa9

--- openarms/control/goto_cart.cpp
+++ openarms/control/goto_cart.cpp
@@ -30,7 +30,7 @@ tf::TransformBroadcaster *g_tf_broadcaster = NULL;
 tf::TransformListener *g_tf_listener = NULL;
 KDL::TreeFkSolverPosFull_recursive *g_fk_solver = NULL;
 KDL::ChainIkSolverPos_NR_JL *g_ik_solver = NULL;
-tf::Transform g_target_origin, g_target;
+tf::Transform /*g_target_origin, */g_target;
 std::vector<double> g_pose;
 
 
@@ -72,9 +72,11 @@ bool ik_tool(tf::Transform t, std::vector<double> &joints)
   KDL::JntArray q_init(7), q(7);
   for (int i = 0; i < 7; i++)
     q_init.data[i] = joints[i];
+  /*
   q_init.data[1] += 0.5 * (-0.7 - joints[1]);
   q_init.data[2] += 0.5 * (-0.7 - joints[2]);
   q_init.data[3] += 0.5 * (-0.1 - joints[3]);
+  */
   // populate F_dest from tf::Transform parameter
   KDL::Frame F_dest;
   tf::TransformTFToKDL(t, F_dest);
@@ -100,9 +102,15 @@ void target_cb(const geometry_msgs::Transform &t_msg)
   //tf::Transform t_bump(btQuaternion::getIdentity(),
   //                     btVector3(d_pose, 0, 0));
   //tf::Transform t_target = t_tool * t_bump;
-  tf::StampedTransform t;
-  //tf::transformMsgToTF(t_msg, t);
-  std::vector<double> j_ik = g_pose;
+  tf::Transform t_input;
+  tf::transformMsgToTF(t_msg, t_input);
+  tf::StampedTransform t(t_input, ros::Time::now(), "world", "ik_target");
+  /*
+  tf::StampedTransform t_target_origin(g_target_origin, ros::Time::now(),
+                                         "torso_link", "target_origin");
+  */
+  std::vector<double> j_ik;// = g_pose;
+  j_ik.resize(7);
   if (g_actual_js.position.size() >= 7)
   {
     for (int i = 0; i < 7; i++)
@@ -110,6 +118,7 @@ void target_cb(const geometry_msgs::Transform &t_msg)
   }
   //t.setRotation(t.getRotation() * btQuaternion(0, 0, 0));
   //t_target_in_torso = g_target_origin * t;
+  /*
   try
   {
     g_tf_listener->lookupTransform("torso_link", "human_tool_link",
@@ -120,28 +129,33 @@ void target_cb(const geometry_msgs::Transform &t_msg)
     ROS_ERROR("%s", ex.what());
     return;
   }
-
-  tf::StampedTransform t_target_in_torso(g_target_origin * t, ros::Time::now(),
+  */
+  tf::StampedTransform t_target_in_torso(t, ros::Time::now(),
                                          "world", "ik_target");
-
   geometry_msgs::TransformStamped target_trans_msg;
+  tf::transformStampedTFToMsg(t_target_in_torso, target_trans_msg);
+  g_tf_broadcaster->sendTransform(target_trans_msg);
+
+/*
+
+  */
   /*
   target_trans.header.frame_id = "torso_link";
   target_trans.header.stamp = ros::Time::now();
   target_trans.child_frame_id = "ik_target";
   */
   //target_trans.transform.translation = g_target_origin * t;
-
-  tf::transformStampedTFToMsg(t_target_in_torso, target_trans_msg);
-  g_tf_broadcaster->sendTransform(target_trans_msg);
-
+/*
+*/
+/*
   tf::StampedTransform t_target_origin(g_target_origin, ros::Time::now(),
                                          "torso_link", "target_origin");
   geometry_msgs::TransformStamped target_origin_msg;
   tf::transformStampedTFToMsg(t_target_origin, target_origin_msg);
   g_tf_broadcaster->sendTransform(target_origin_msg);
-
-  ik_tool(g_target_origin * t, j_ik);
+*/
+//  ik_tool(g_target_origin * t, j_ik);
+  ik_tool(t, j_ik);
   g_js.header.stamp = ros::Time::now();
   g_js.position = j_ik;
   g_joint_pub->publish(g_js);
@@ -211,15 +225,15 @@ int main(int argc, char **argv)
   KDL::ChainIkSolverVel_pinv ik_solver_vel(chain);
   KDL::JntArray q_min(7), q_max(7);
   q_min.data[0] = -.7;
-  q_min.data[1] = -1.0;
-  q_min.data[2] = -1.57;
+  q_min.data[1] = -2.5;
+  q_min.data[2] = -2;
   q_min.data[3] = -1.6;
   q_min.data[4] = -3.14;
   q_min.data[5] = -1.57;
   q_min.data[6] = -3.14;
   q_max.data[0] = 1.0;
   q_max.data[1] = 0.0;
-  q_max.data[2] = 1.57;
+  q_max.data[2] = 2;
   q_max.data[3] = 1.0;
   q_max.data[4] = 3.14;
   q_max.data[5] = 1.57;
@@ -248,7 +262,7 @@ int main(int argc, char **argv)
   //btQuaternion target_quat;
   //target_quat.setEuler(1.57, -1.57, 0);
   //g_target_origin.setRotation(target_quat);
-  g_target_origin = btTransform(btQuaternion::getIdentity(), btVector3(0, 0.1, 0));
+  //g_target_origin = btTransform(btQuaternion::getIdentity(), btVector3(0, 0.1, 0));
   //tf::Transform(btQuaternion::getIdentity(), btVector3(0, 0, 0));
 
   while (ros::ok())
