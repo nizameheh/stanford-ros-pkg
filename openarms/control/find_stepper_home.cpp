4ca6c0737d7467d1ddddc5c47363061ee2bf0543
morganquigley <morganquigley@a5c9a0f3-a0d0-8808-10fc-6cde0bab7d8d>
2010-08-20 01:34:20 +0000

    hacking around

--- openarms/control/find_stepper_home.cpp
+++ openarms/control/find_stepper_home.cpp
@@ -7,12 +7,12 @@ double g_gripper_target = 0;
 ros::Publisher *g_actuator_pub = NULL;
 openarms::ArmActuators g_actuators;
 //double g_stepper_vel[4];
-uint16_t g_encoder_target[4] = {0, 0, 0, 0};
+uint16_t g_encoder_target[4] = {9000, 0, 0, 0};
 
 void sensors_cb(const openarms::ArmSensors::ConstPtr &msg)
 {
-  const double MAX_VEL_BIGDOGS = 1000, MAX_VEL_LITTLEDOGS = 1000;
-  const double MAX_ACCEL_PER_SEC = 20000;
+  const double MAX_VEL_BIGDOGS = 3000, MAX_VEL_LITTLEDOGS = 3000;
+  const double MAX_ACCEL_PER_SEC = 30000;
   static ros::Time s_prev_time;
   static bool s_prev_time_init = false;
   static double s_prev_encoder[4];
@@ -31,6 +31,18 @@ void sensors_cb(const openarms::ArmSensors::ConstPtr &msg)
     double dt = (t - s_prev_time).toSec();
     s_prev_time = t;
     const int32_t MAX_ACCEL = (int32_t)(MAX_ACCEL_PER_SEC * dt);
+    double vel[4];
+    for (int i = 0; i < 4; i++)
+    {
+      vel[i] = msg->encoder[i] - s_prev_encoder[i];
+      s_prev_encoder[i] = msg->encoder[i];
+      if (vel[i] < -8000)
+        vel[i] += 10000;
+      else if (vel[i] > 8000)
+        vel[i] -= 10000;
+    }
+    
+    //printf("vel[0] = %.0f\n", vel[0]);
 
     for (int i = 0; i < 4; i++)
     {
@@ -41,15 +53,26 @@ void sensors_cb(const openarms::ArmSensors::ConstPtr &msg)
         continue;
       }
       */
-      double err = (double)g_encoder_target[i] - (double)msg->encoder[i];
-      if (err < -5000)
-        err += 10000;
-      else if (err > 5000)
-        err -= 10000;
-      double vel = msg->encoder[i] - s_prev_encoder[i];
+      double pos_err = (double)g_encoder_target[i] - (double)msg->encoder[i];
+      if (pos_err < -5000)
+        pos_err += 10000;
+      else if (pos_err > 5000)
+        pos_err -= 10000;
 
-      double desired_vel = 1*err; // + 20*(0 - vel);
-      s_prev_encoder[i] = msg->encoder[i];
+      double desired_vel = 0;
+      if (i == 0)
+        desired_vel = 97*vel[0];
+      else if (i == 1)
+      {
+        desired_vel = 0.8096*97.0*vel[0];//50*vel[0] + 150*(vel[1] + 0.25*vel[0]);
+        //desired_vel = 80*vel;
+      }
+      else if (i == 2)
+        desired_vel = 100*vel[2];
+      else if (i == 3)
+        desired_vel = 100*vel[3];
+
+      //double desired_vel = 50*vel; //1*pos_err + 1*(0 - vel);
 
       /*
       if (err < -10)
@@ -131,5 +154,15 @@ int main(int argc, char **argv)
     loop_rate.sleep();
     ros::spinOnce();
   }
+
+  for (int i = 0; i < 4; i++)
+  {
+    g_actuators.stepper_vel[i] = 0;
+    g_actuators.servo_torque[i] = 0;
+  }
+  g_actuator_pub->publish(g_actuators);
+  ros::spinOnce();
+  ros::Duration(1.0).sleep();
+  return 0;
 }
 
