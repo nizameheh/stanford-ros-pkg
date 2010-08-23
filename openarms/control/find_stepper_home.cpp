baac3cce5e0a043e1814f9e999cba02677f39f6f
aasbeck <aasbeck@a5c9a0f3-a0d0-8808-10fc-6cde0bab7d8d>
2010-08-23 00:20:37 +0000

    includes D-term damping

--- openarms/control/find_stepper_home.cpp
+++ openarms/control/find_stepper_home.cpp
@@ -16,13 +16,19 @@ void sensors_cb(const openarms::ArmSensors::ConstPtr &msg)
   static ros::Time s_prev_time;
   static bool s_prev_time_init = false;
   static double s_prev_encoder[4];
+  static double s_prev_vel[4];
+  
+  
 
   if (!s_prev_time_init)
   {
     s_prev_time_init = true;
     s_prev_time = ros::Time::now();
     for (int i = 0; i < 4; i++)
+    {
       s_prev_encoder[i] = 0;
+      s_prev_vel[i] = 0;
+    }
     return;
   }
   else
@@ -32,14 +38,23 @@ void sensors_cb(const openarms::ArmSensors::ConstPtr &msg)
     s_prev_time = t;
     const int32_t MAX_ACCEL = (int32_t)(MAX_ACCEL_PER_SEC * dt);
     double vel[4];
+    double accel_encoder[4];
     for (int i = 0; i < 4; i++)
     {
-      vel[i] = msg->encoder[i] - s_prev_encoder[i];
+      //vel[i] = msg->encoder[i] - s_prev_encoder[i];
+      
+      // try add a low-pass filter on this to make it smoother..
+      vel[i] = 0.1*(msg->encoder[i] - s_prev_encoder[i]) + 0.9*s_prev_vel[i];
+      
       s_prev_encoder[i] = msg->encoder[i];
       if (vel[i] < -8000)
         vel[i] += 10000;
       else if (vel[i] > 8000)
         vel[i] -= 10000;
+      
+      accel_encoder[i] = vel[i] - s_prev_vel[i];
+      s_prev_vel[i] = vel[i];
+      
     }
     
     //printf("vel[0] = %.0f\n", vel[0]);
@@ -53,34 +68,53 @@ void sensors_cb(const openarms::ArmSensors::ConstPtr &msg)
         continue;
       }
       */
+
       double pos_err = (double)g_encoder_target[i] - (double)msg->encoder[i];
       if (pos_err < -5000)
         pos_err += 10000;
       else if (pos_err > 5000)
         pos_err -= 10000;
 
-      double desired_vel = 0;
+      /*
+      if ( i==0 )
+        printf("pos_err[0] = %f\n",pos_err);
+      */
+      
+      
+      //               0    1    2    3
+      double Ki[4] = {0.0, 0.0, 0.0, 0.0};
+      double Kp[4] = {0.0, 0.0, 0.0, 0.0};
+      double Kd[4] = {1000.0, 0.0, 0.0, 0.0};
+      
+      // for now, always assume that we have g_encoder_target==const so the derivative of it is 0.
+      // otherwise we need to add more terms!
+      
+      double desired_vel[4] = {0.0, 0.0, 0.0, 0.0};
+      //double desired_vel = 0;
+      
       if (i == 0)
-        desired_vel = 97*vel[0];
+        desired_vel[0] = Ki[0]*pos_err + Kp[0]*(0.0-vel[0]) + Kd[0]*(0.0-accel_encoder[0]);
+        
+        //desired_vel = 97*vel[0];
       else if (i == 1)
       {
-        desired_vel = 0.8096*97.0*vel[0];//50*vel[0] + 150*(vel[1] + 0.25*vel[0]);
+        // FIXME TODO should really make it use the actual velocity after the acceleration and 
+        // velocity limits done down below..
+        desired_vel[1] = 0.8096*desired_vel[0] + 
+                        Ki[1]*pos_err + Kp[1]*(0.0-vel[1]) + Kd[1]*(0.0-accel_encoder[1]);
+        //desired_vel = 0.8096*97.0*vel[0];//50*vel[0] + 150*(vel[1] + 0.25*vel[0]);
         //desired_vel = 80*vel;
       }
       else if (i == 2)
-        desired_vel = 100*vel[2];
+        desired_vel[2] = 0; //100*vel[2];
       else if (i == 3)
-        desired_vel = 100*vel[3];
+        desired_vel[3] = 0;  //100*vel[3];
 
       //double desired_vel = 50*vel; //1*pos_err + 1*(0 - vel);
 
-      /*
-      if (err < -10)
-        desired_vel = -100;
-      else if (err > 10)
-        desired_vel = 100;
-      */
-      double accel = desired_vel - g_actuators.stepper_vel[i];
+      
+
+      double accel = desired_vel[i] - g_actuators.stepper_vel[i];
       // enforce acceleration limit
       if (accel > MAX_ACCEL)
         accel = MAX_ACCEL;
@@ -94,10 +128,11 @@ void sensors_cb(const openarms::ArmSensors::ConstPtr &msg)
       else if (g_actuators.stepper_vel[i] < -MAX_VEL)
         g_actuators.stepper_vel[i] = -MAX_VEL;
 
-      /*
-      printf("vel = %.0f,  desired = %.0f, cmd = %d\n",
-             vel, desired_vel, g_actuators.stepper_vel[i]);
-      */
+      
+      if ( i==0 ) {
+        printf("vel[0] = %f,  desired[0] = %f, cmd = %d\n",
+             vel[0], desired_vel[0], g_actuators.stepper_vel[i]);
+      }      
       /*
          if (abs(g_actuators.stepper_vel[i]) < 20)
          g_actuators.stepper_vel[i] = 0;
