310bde0269fe50d1c27ad491c0f6701835782ea8
morganquigley <morganquigley@a5c9a0f3-a0d0-8808-10fc-6cde0bab7d8d>
2010-08-23 06:48:31 +0000

    improved state estimation and gamepad master

--- trunk/openarms/master/master_gamepad.cpp
+++ trunk/openarms/master/master_gamepad.cpp
@@ -7,18 +7,21 @@
 #include "geometry_msgs/Transform.h"
 #include "LinearMath/btTransform.h"
 #include "tf/transform_datatypes.h"
+#include "tf/transform_broadcaster.h"
 #include "joy/Joy.h"
 
+const static int32_t MAX_BUTTONS = 10;
 ros::Publisher *g_target_pub = NULL;
-double g_joy_axes[4];
+static double g_joy_axes[4];
+static int    g_joy_buttons[MAX_BUTTONS];
 
 void joy_cb(const joy::Joy::ConstPtr &joy_msg)
 {
   if (joy_msg->axes.size() >= 4)
-  {
     for (int i = 0; i < 4; i++)
       g_joy_axes[i] = joy_msg->axes[i];
-  }
+  for (int i = 0; i < MAX_BUTTONS && i < (int)joy_msg->buttons.size(); i++)
+    g_joy_buttons[i] = joy_msg->buttons[i];
 }
 
 int main(int argc, char **argv)
@@ -30,23 +33,49 @@ int main(int argc, char **argv)
   g_target_pub = &tf_pub;
   for (int i = 0; i < 4; i++)
     g_joy_axes[i] = 0;
+  for (int i = 0; i < MAX_BUTTONS; i++)
+    g_joy_buttons[i] = 0;
   puts("greetings. ctrl-c to exit.");
+
   //t = tf::Transform(btQuaternion::getIdentity(), btVector3(0,0,0));
   
-  ros::Rate loop_rate(100);
+  ros::Rate loop_rate(50);
 
   geometry_msgs::Transform tf_msg;
-  //tf::Transform target_tf;
-  btVector3 target_vec(0,0,0);
+  btVector3 target_vec(0, 0.75, -0.2);
+
+  tf::TransformBroadcaster tf_broadcaster;
+  btQuaternion orient(btQuaternion(btVector3(1, 0, 0), -2.5)); // *
+                      //btQuaternion(btVector3(1, 0, 0), 2.50));
 
   while (ros::ok())
   {
-    target_vec += 0.001 * btVector3(g_joy_axes[0],
-                                    g_joy_axes[1],
-                                    g_joy_axes[2]);
+    if (!g_joy_buttons[5])
+    {
+      double speed = (g_joy_buttons[4] ? 0.005 : 0.001);
+      target_vec += speed * btVector3(-g_joy_axes[0],
+                                       g_joy_axes[1],
+                                       g_joy_axes[3]);
+    }
+    else
+    {
+      double speed = (g_joy_buttons[4] ? 0.040 : 0.01);
+      orient = btQuaternion(btVector3(1, 0, 0), speed * g_joy_axes[1]) * orient;
+      orient = btQuaternion(btVector3(0, 0, 1), speed * g_joy_axes[0]) * orient;
+      orient = orient * btQuaternion(btVector3(0, 0, 1), speed * g_joy_axes[2]);
+      orient.normalize();
+    }
+    /*
     tf::transformTFToMsg(tf::Transform(btQuaternion::getIdentity(), target_vec),
                          tf_msg);
-    g_target_pub->publish(tf_msg);
+    */
+    tf::StampedTransform t_target_in_world(tf::Transform(orient, target_vec),
+                                           ros::Time::now(),
+                                           "world", "ik_target");
+    geometry_msgs::TransformStamped t_target_msg;
+    tf::transformStampedTFToMsg(t_target_in_world, t_target_msg);
+    tf_broadcaster.sendTransform(t_target_msg);
+    //g_target_pub->publish(tf_msg);
     loop_rate.sleep();
     ros::spinOnce();
   }
--- trunk/openarms/state/oa-state.cpp
+++ trunk/openarms/state/oa-state.cpp
@@ -6,27 +6,21 @@
 
 static double g_joint_pos[8];
 ros::Publisher *g_joint_pub = NULL;
-static int32_t g_stepper_offsets[4], g_servo_offsets[3];
+static double g_encoder_offsets[7], g_gear_ratios[3], g_encoder_signs[7];
 int32_t g_servo_wraps[3];
 enum servo_wrap_t { COMING_FROM_LOW, COMING_FROM_HIGH, NOT_IN_WRAP} g_servo_in_wrap[3];
 
 void sensors_cb(const openarms::ArmSensors::ConstPtr &sensors)
 {
-/*
-  if (!g_offset_init_complete)
+  for (int i = 0; i < 4; i++)
   {
-    g_offset_init_complete = true;
-    for (int i = 0; i < 4; i++)
-      g_stepper_offsets[i] = sensors->pos[i];
-    for (int i = 0; i < 3; i++)
-      g_servo_offsets[i] = sensors->pos[i+4];
+    int enc = sensors->encoder[i];
+    if (enc > 5000)
+      enc -= 10000;
+    g_joint_pos[i] = enc / 10000.0 * 2 * M_PI * g_encoder_signs[i] + 
+                     g_encoder_offsets[i];
   }
-*/
-  // 10-microstep, 1.8 deg/step
-  g_joint_pos[0] = (sensors->pos[0] - g_stepper_offsets[0]) / 2.0 / 10.0 * 1.8 * 3.1415 / 180.0 / 6.5; 
-  g_joint_pos[1] = (sensors->pos[1] - g_stepper_offsets[1]) / 2.0 / 10.0 * 1.8 * 3.1415 / 180.0 / 10.133 - g_joint_pos[0] / 1.97 - 1.57; 
-  g_joint_pos[2] = (sensors->pos[2] - g_stepper_offsets[2]) / 2.0 / 10.0 * 1.8 * 3.1415 / 180.0 / 14; //  + 1.57;
-  g_joint_pos[3] = (sensors->pos[3] - g_stepper_offsets[3]) / 2.0 / 10.0 * 1.8 * 3.1415 / 180.0 / 14; // mounted too low now, init with elbow bent  - 1.57; // assume we init with elbow straight down
+
 
   const int32_t DEADBAND_WIDTH = 8;
   const int32_t WRAP_THRESH_LOW = DEADBAND_WIDTH;
@@ -59,17 +53,9 @@ void sensors_cb(const openarms::ArmSensors::ConstPtr &sensors)
       else
         pos = 0;
     }
-/*
-    if (g_servo_in_wrap[i] == COMING_FROM_HIGH)
-    {
-      if (pos > WRAP_THRESH_LOW
-    }
-    if (g_servo_in_wrap[i] != NOT_IN_WRAP &&
-        (pos > NOT_IN_WRAP_THRESH && pos < 1024 - NOT_IN_WRAP_THRESH))
-    {
-    }
-*/
-    g_joint_pos[i+4] = (pos - g_servo_offsets[i] + 1024 * g_servo_wraps[i]) * 2 * 3.1415 / 1023.0 / 2.8;
+    g_joint_pos[i+4] = g_encoder_offsets[i+4] +
+                       (pos + 1024 * g_servo_wraps[i]) * g_encoder_signs[i+4] *
+                        2 * 3.1415 / 1023.0 / g_gear_ratios[i];
     /*
     if (i == 1)
       printf("%8d %4d %4d %4d %.3f\n", 
@@ -78,17 +64,32 @@ void sensors_cb(const openarms::ArmSensors::ConstPtr &sensors)
              g_joint_pos[i+4]);
     */
   }
+  /*
+  printf("%+5d %+5d %+5d\n",
+         g_servo_wraps[0], g_servo_wraps[1], g_servo_wraps[2]);
+  */
+  
+  // enforce joint limits on the wrap count
   // wrist pitch. enforce joint limits on wrap count
-  if (g_joint_pos[5] < -2.2)
+
+  if (g_joint_pos[5] < -2.5)
   {
     ROS_ERROR("woah, wrist overwrap negative");
     g_servo_wraps[1]++;
   }
-  else if (g_joint_pos[5] > 2.4)
+  else if (g_joint_pos[5] > 2.5)
   {
     ROS_ERROR("woah, wrist overwrap positive");
     g_servo_wraps[1]--;
   }
+
+  // enforce joint limits on forearm roll
+  /*
+  if (g_joint_pos[4] < -3.0)
+    g_servo_wraps[0]++;
+  else if (g_joint_pos[4] > 3.1)
+    g_servo_wraps[0]--;
+  */
 
   // handle the gripper now
   g_joint_pos[7] = (sensors->pos[7] - 216) / (371.0 - 216.0);
@@ -133,14 +134,20 @@ int main(int argc, char **argv)
   
   for (uint32_t i = 0; i < 8; i++)
     g_joint_pos[i] = 0;
-  for (uint32_t i = 0; i < 4; i++)
-    g_stepper_offsets[i] = 0;
+  for (uint32_t i = 0; i < 7; i++)
+  {
+    g_encoder_offsets[i] = 0;
+    g_encoder_signs[i] = 1;
+  }
   for (uint32_t i = 0; i < 3; i++)
   {
-    g_servo_offsets[i] = 0;
+    g_gear_ratios[i] = 0;
     g_servo_wraps[i] = 0;
     g_servo_in_wrap[i] = NOT_IN_WRAP;
   }
+  g_servo_wraps[0] = -1;
+  g_servo_wraps[1] = -1;
+  g_servo_wraps[2] = -1;
 
   FILE *f = fopen(argv[1], "r");
   if (!f)
@@ -149,7 +156,35 @@ int main(int argc, char **argv)
     return 1;
   }
   // read in the calibration data...
+  while (!feof(f))
+  {
+    char line[1000];
+    if (!fgets(line, sizeof(line), f))
+      break;
+    if (!strlen(line))
+      continue;
+    char *key = strtok(line, " \n");
+    char *token = strtok(NULL, " \n");
+    if (!strcmp(key, "encoder_offsets"))
+      for (int i = 0; token && i < 7; i++, token = strtok(NULL, " \n"))
+        g_encoder_offsets[i] = atof(token);
+    else if (!strcmp(key, "gear_ratios"))
+      for (int i = 0; token && i < 3; i++, token = strtok(NULL, " \n"))
+        g_gear_ratios[i] = atof(token);
+    else if (!strcmp(key, "encoder_signs"))
+      for (int i = 0; token && i < 7; i++, token = strtok(NULL, " \n"))
+        g_encoder_signs[i] = atof(token);
+    else
+      printf("unrecognized key: [%s]\n", token);
+  }
   fclose(f);
+  printf("enc offsets: ");
+  for (int i = 0; i < 7; i++)
+    printf(" %8.2f", g_encoder_offsets[i]);
+  printf("\ngear ratios: ");
+  for (int i = 0; i < 3; i++)
+    printf(" %8.2f", g_gear_ratios[i]);
+  printf("\n");
   
   ros::NodeHandle n;
 
@@ -157,14 +192,13 @@ int main(int argc, char **argv)
   g_joint_pub = &joint_pub; // ugly ugly
   ros::Subscriber sensor_sub = n.subscribe("arm_sensors", 1, sensors_cb);
   tf::TransformBroadcaster tf_broadcaster;
-  ros::Rate loop_rate(100);
+  ros::Rate loop_rate(30); // just fast enough to be new enough so tf is happy
   geometry_msgs::TransformStamped world_trans;
   world_trans.header.frame_id = "world";
   world_trans.child_frame_id = "torso_link";
   world_trans.transform.translation.x = 0;
   world_trans.transform.translation.y = 0;
   world_trans.transform.translation.z = 0;
-  //world_trans.transform.rotation = tf::createQuaternionMsgFromRollPitchYaw(90*RAD2DEG,0,0);
   world_trans.transform.rotation = tf::createQuaternionMsgFromRollPitchYaw(0,0,0);
 
   while (ros::ok())
