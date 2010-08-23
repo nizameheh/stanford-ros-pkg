58dfdb057e8b24571c31638c6433ecdebd7010a8
morganquigley <morganquigley@a5c9a0f3-a0d0-8808-10fc-6cde0bab7d8d>
2010-08-23 00:48:17 +0000

    little program for manually navigating through a waypoint file

--- openarms/control/waypoint_keyboard.cpp
+++ openarms/control/waypoint_keyboard.cpp
@@ -1,17 +1,65 @@
 // copyright 2010 morgan quigley bsd license blah blah
-// lots of code lifted from the teleop_head_keyboard program by Kevin Watts
+// some code lifted from the teleop_head_keyboard program by Kevin Watts
 #include <cstdlib>
 #include <cstdio>
 #include <termios.h>
+#include <ros/ros.h>
+#include <openarms/SetJointTarget.h>
+#include <sensor_msgs/JointState.h>
+#include <vector>
+#include <cstring>
+using std::vector;
 
 struct termios cooked, raw; // mmm makes me hungry
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "master_keyboard"); // remember he-man?
+  ros::init(argc, argv, "waypoint_keyboard");
+  if (argc != 2)
+  {
+    ROS_INFO("usage: waypoint_keyboard WAYPOINTFILE");
+    return 1;
+  }
+  FILE *f = fopen(argv[1], "r");
+  if (!f)
+  {
+    ROS_ERROR("woah there. couldn't open %s", argv[1]);
+    return 1;
+  }
+  vector<sensor_msgs::JointState> js_vec;
+  while (!feof(f))
+  {
+    char line[1000];
+    if (!fgets(line, sizeof(line), f))
+      break;
+    if (!strlen(line))
+      continue;
+    sensor_msgs::JointState js;
+    js.name.resize(8);
+    js.name[0] = "shoulder1";
+    js.name[1] = "shoulder2";
+    js.name[2] = "shoulder3";
+    js.name[3] = "elbow";
+    js.name[4] = "wrist1";
+    js.name[5] = "wrist2";
+    js.name[6] = "wrist3";
+    js.name[7] = "gripper";
+    js.position.resize(8);
+    for (int i = 0; i < 8; i++)
+      js.position[i] = 0;
+    const char *token = strtok(line, " \n");
+    for (int i = 0; token; i++, token = strtok(NULL, " \n"))
+      js.position[i] = atof(token);
+    printf("%c: ", (uint8_t)js_vec.size() + 'a');
+    js_vec.push_back(js);
+    for (int i = 0; i < 8; i++)
+      printf(" %+08.2f", js.position[i]);
+    printf("\n");
+  }
+
   ros::NodeHandle n;
-  ros::Publisher ik_req_pub = n.advertise<openarms::ArmIKRequest>("ik_request", 1);
-  char c;
+  ros::Publisher pub = n.advertise<sensor_msgs::JointState>("target_joints", 1);
+
   tcgetattr(0, &cooked);
   memcpy(&raw, &cooked, sizeof(struct termios));
   raw.c_lflag &= ~(ICANON | ECHO);
@@ -19,15 +67,10 @@ int main(int argc, char **argv)
   raw.c_cc[VMIN] = 0;
   tcsetattr(0, TCSANOW, &raw);
   puts("greetings. ctrl-c to exit.");
-  //tf::Transform t(btQuaternion::getIdentity(), btVector3(0, 0, 0));
-  btVector3 target_vec(-0.2, 0.45, -0.3);
-  btQuaternion target_quat(btQuaternion(btVector3(1, 0, 0), 3.14));
-  openarms::ArmIKRequest ik_msg;
 
-  const double DELTA = 0.01, QUAT_DELTA = 0.05;
-  
   while (ros::ok())
   {
+    char c;
     ssize_t nread = read(0, &c, 1);
     if (nread < 0)
     {
@@ -38,10 +81,16 @@ int main(int argc, char **argv)
       c = 0;
     if (c >= 'a' && c <= 'z')
     {
-      printf("pressed: %c\n", c);
+      int js_idx = c - 'a';
+      if (js_idx >= 0 && js_idx < (int)js_vec.size())
+      {
+        ROS_INFO("sent waypoint %c", 'a' + js_idx);
+        pub.publish(js_vec[js_idx]);
+      }
+      else
+        ROS_INFO("only a-%c are defined in this file",
+                 'a'+(int)js_vec.size()-1);
     }
-    if (c == 0x2a)
-      break;
     /*
     tf::transformTFToMsg(tf::Transform(target_quat, target_vec), ik_msg.t);
     ik_req_pub.publish(ik_msg);
