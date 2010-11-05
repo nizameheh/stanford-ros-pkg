edeb244e19f7229a25aa820e1e8880b8db3cf2ff
sjh.hkust <sjh.hkust@a5c9a0f3-a0d0-8808-10fc-6cde0bab7d8d>
2010-11-05 01:28:14 +0000

    detect three bottles

--- recyclerbot/src/table_detector.cpp
+++ recyclerbot/src/table_detector.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 TableDetector::TableDetector()
 {
-
+	
 }
 
 /*
@@ -24,15 +24,19 @@ sensor_msgs/ChannelFloat32[] channels
  
 void TableDetector::process_cloud(
 sensor_msgs::PointCloud& original_msg,
-sensor_msgs::PointCloud* filtered_msg
+sensor_msgs::PointCloud* filtered_msg,
+visualization_msgs::Marker* marker_msg
 )
 {
 	vector<geometry_msgs::Point32>& pointCloud = original_msg.points;
+	sensor_msgs::PointCloud pointsAboveTable;
   int i = 0;
 	int j = 0;  
   double maxZ = 0;
   double minZ = 100;
   int pointCloudNum = pointCloud.size();
+  int* assigned = NULL;
+  
   // find max and min z value
   for (i = 0; i < pointCloudNum; i++) 
   {
@@ -69,7 +73,7 @@ sensor_msgs::PointCloud* filtered_msg
 	for (i = 0; i < pointCloudNum; i++)
 	{
 		//if ((pointCloud[i].z >= (double)maxSlot*2/100)&&(pointCloud[i].z < ((double)maxSlot+1)*2/100))
-		if (pointCloud[i].z >= ((double)maxSlot+1)*2/100)
+		if (pointCloud[i].z >= ((double)maxSlot + 1) * 2 / 100)
 		{
 			p_.x = pointCloud[i].x;
 			p_.y = pointCloud[i].y;
@@ -77,28 +81,141 @@ sensor_msgs::PointCloud* filtered_msg
 			ch0_.values.push_back(original_msg.channels[0].values[i]);
 			ch1_.values.push_back(original_msg.channels[1].values[i]);
 			ch2_.values.push_back(original_msg.channels[2].values[i]);
-			filtered_msg->points.push_back(p_);
+			pointsAboveTable.points.push_back(p_);
 			j++;
 			
-			if (j > newPointNum) 
-			{
-			//	cout << "j>newPointNum" << endl;
-			//	break;
-			}
 		}
 	}
+	pointsAboveTable.channels.push_back(ch0_);
+	pointsAboveTable.channels.push_back(ch1_);
+	pointsAboveTable.channels.push_back(ch2_);
+
+	// now start to find cylinders!
+	vector<long unsigned int> clusterId;
+	geometry_msgs::Point32 center;
+	double radius = 0;
+	
+//	int k = 6000;
+//	find_cluster(filtered_msg->points, k, clusterId);
+
+	ch0_.name = 'r';
+	ch1_.name = 'g';
+	ch2_.name = 'b';
+	ch0_.values.clear();
+	ch1_.values.clear();
+	ch2_.values.clear();
+
+	vector<geometry_msgs::Point32> tempCloud;
+	for (int cylId = 0; cylId < 3; cylId++)
+	{
+		// find first bottle
+		find_cylinder(pointsAboveTable.points, center, radius, clusterId);
+		// set cylinder marker
+		marker_msg[cylId].pose.position.x = center.x;
+		marker_msg[cylId].pose.position.y = center.y;
+		marker_msg[cylId].pose.position.z = center.z;
+		marker_msg[cylId].pose.orientation.x = 0.0;
+		marker_msg[cylId].pose.orientation.y = 0.0;
+		marker_msg[cylId].pose.orientation.z = 0.0;
+		marker_msg[cylId].pose.orientation.w = 1.0;
+		marker_msg[cylId].scale.x = radius * 2;
+		marker_msg[cylId].scale.y = radius * 2;
+		marker_msg[cylId].scale.z = 0.1;
+		
+		// set channels
+		int filterPointNum = pointsAboveTable.points.size();
+		if (assigned != NULL)	delete assigned;
+		assigned = new int[filterPointNum];
+		for (i = 0; i < filterPointNum; i++) assigned[i] = 0;
+	
+		for (i = 0; i < clusterId.size(); i++)
+		{
+			filtered_msg->points.push_back(pointsAboveTable.points[clusterId[i]]);
+			ch0_.values.push_back(cylId == 0 ? 1:0);
+			ch1_.values.push_back(cylId == 1 ? 1:0);
+			ch2_.values.push_back(cylId == 2 ? 1:0);
+			assigned[clusterId[i]] = 1;
+		}
+		
+		// update point cloud
+		tempCloud.clear();
+		for (i = 0; i < filterPointNum; i++)
+		{
+			if (assigned[i] == 0) tempCloud.push_back(pointsAboveTable.points[i]);
+		}
+	
+		pointsAboveTable.points.clear();
+		pointsAboveTable.points = tempCloud;
+	}
+	
 	filtered_msg->channels.push_back(ch0_);
 	filtered_msg->channels.push_back(ch1_);
-	filtered_msg->channels.push_back(ch2_);
+	filtered_msg->channels.push_back(ch2_);	
+	
+	cout<<"remaining points: "<<tempCloud.size()<<endl;
+	
+}
 
-	vector<long unsigned int> clusterId;
-	int k = 6000;
-	find_cluster(filtered_msg->points, k, clusterId);
-	for (i = 0; i < k; i++)
+void TableDetector::find_cylinder(vector<geometry_msgs::Point32>& pointCloud,
+								   geometry_msgs::Point32& center,
+								   double& r,
+									 vector<long unsigned int>& clusterId)
+{
+	// RANSAC on 3 points to fit circle model
+	int iter = 2000;
+	int i = 0;
+	int j = 0;
+	int p1, p2, p3;
+	int consensus = 0;
+	int consensus_t = 0; // temp variable
+	geometry_msgs::Point32 center_t; // temp variable
+	double r_t = 0; // temp variable
+	double error = 0;
+	double margin = 0.005;
+	int size = pointCloud.size();
+	
+	for (i = 0; i < iter; i++)
 	{
-		filtered_msg->channels[0].values[clusterId[i]] = 1;
-	//	filtered_msg->channels[1].values[clusterId[i]] = 0.5;
-	//	filtered_msg->channels[2].values[clusterId[i]] = 0.5;
+		// pick three random points
+		p1 = int(__drand48__() * (size - 1));
+		p2 = int(__drand48__() * (size - 1));
+		p3 = int(__drand48__() * (size - 1));
+		if ((p1 == p2) || (p1 == p3) || (p2 == p3)) {i--;continue;}
+		// find the circle
+		find_circle(pointCloud[p1], pointCloud[p2], pointCloud[p3], center_t, r_t);
+		// evaluate this circle
+		if (r_t > 0.07) {i--;continue;}
+		consensus_t = 0;
+		for (j = 0; j < size; j++)
+		{
+			error = distance(pointCloud[j], center_t) - r_t;
+			if ((error < margin) && (error > -margin)) consensus_t++;
+		}
+		
+		
+		
+		
+		// update center and radius
+		if (consensus_t > consensus)
+		{
+			center.x = center_t.x;
+			center.y = center_t.y;			
+			center.z = (pointCloud[p1].z + pointCloud[p2].z + pointCloud[p3].z) / 3;
+			r = r_t;
+			consensus = consensus_t;
+		}
+	}
+			
+	clusterId.clear();
+	for (j = 0; j < size; j++)
+	{
+		error = distance(pointCloud[j], center) - r;
+		if ((error < margin*2) && (error > -margin*2))
+		{
+			clusterId.push_back(j);
+		}
 	}
 }
+
+
 
--- recyclerbot/src/table_detector_node.cpp
+++ recyclerbot/src/table_detector_node.cpp
@@ -15,6 +15,7 @@ public:
   ros::NodeHandle n;
 	ros::Subscriber ntPoint_sub; // nt = narrow textured point cloud
 	ros::Publisher filteredPoints_pub; // publish processed points
+	ros::Publisher cylMarker_pub; // publish cylinders
 	tf::TransformListener* tran; // narrow textured to Footprint
 	ros::Time preStamp;
 	TableDetector tableDetector;
@@ -22,6 +23,7 @@ public:
   TableDetectorNode(ros::NodeHandle &_n) : n(_n)
   {
     filteredPoints_pub = n.advertise<sensor_msgs::PointCloud>("/filtered_points", 1);
+    cylMarker_pub = n.advertise<visualization_msgs::Marker>("/bottle_marker", 3);
     ntPoint_sub = n.subscribe<sensor_msgs::PointCloud>("/narrow_stereo_textured/points", 1, &TableDetectorNode::nt_cb, this);
     tran = new tf::TransformListener(n, ros::Duration(2.0));
     preStamp = ros::Time::now();
@@ -35,47 +37,67 @@ public:
     delete tran;
   }
   
-  void nt_cb(const sensor_msgs::PointCloud::ConstPtr &msg) // narrow textured call back
-  {
-  	// tranform point cloud into /base_footprint frame
-  	sensor_msgs::PointCloud transformedPoints;
-  	if (preStamp >= msg->header.stamp) 
-  	{
-  		cout<<"flush listener"<<endl;
-  		preStamp = msg->header.stamp;
-  		tran->clear();
-  		return;
-  	}
-  	preStamp = msg->header.stamp;
-  	try 
-  	{
-  		tran->transformPointCloud("/base_footprint", *msg, transformedPoints);
-    } 
-    catch (tf::TransformException& ex) 
-    {
-    	return;
-    }
-    
-  	sensor_msgs::PointCloud filtered_msg;
-  	// copy header info into new message
-		filtered_msg.header.frame_id = "/base_footprint";//msg->header.frame_id;
-		filtered_msg.header.stamp = ros::Time::now();
-		filtered_msg.header.seq = msg->header.seq;
-		// get table point cloud
-  	tableDetector.process_cloud(transformedPoints, &filtered_msg);
-  	//cout<<filtered_msg.header.seq<<endl;
-  	//cout<<"size: "<<filtered_msg.points.size()<<endl;
-		if (n.ok()) filteredPoints_pub.publish(filtered_msg);
-//		ros::spinOnce();
-/*    geometry_msgs::PointStamped m;
-  	for (int i = 0; i < msg->points.length(); i++) 
-  	{
-  		tran->transformPoint("/base_footprint", msg->points(i), m);
-  		transformedPoints.push_back();
-  	}*/
-  }
+  void nt_cb(const sensor_msgs::PointCloud::ConstPtr &msg); // narrow textured call back
 };
 
+void TableDetectorNode::nt_cb(const sensor_msgs::PointCloud::ConstPtr &msg) // narrow textured call back
+{
+	// tranform point cloud into /base_footprint frame
+	sensor_msgs::PointCloud transformedPoints;
+	if (preStamp >= msg->header.stamp)
+	{
+		cout<<"flush listener"<<endl;
+		preStamp = msg->header.stamp;
+		tran->clear();
+		return;
+	}
+	preStamp = msg->header.stamp;
+	try 
+	{
+		tran->transformPointCloud("/base_footprint", *msg, transformedPoints);
+  } 
+  catch (tf::TransformException& ex) 
+  {
+  	return;
+  }
+  
+	sensor_msgs::PointCloud filtered_msg;
+	visualization_msgs::Marker marker_msg[3];
+	
+	// copy header info into new message
+	filtered_msg.header.frame_id = "/base_footprint";//msg->header.frame_id;
+	filtered_msg.header.stamp = ros::Time::now();
+	filtered_msg.header.seq = msg->header.seq;
+	
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		marker_msg[i].header.frame_id = "/base_footprint";
+		marker_msg[i].header.stamp = ros::Time::now();
+		marker_msg[i].ns = "bottle";
+		marker_msg[i].id = i;
+		marker_msg[i].type = visualization_msgs::Marker::CYLINDER;
+		marker_msg[i].action = visualization_msgs::Marker::ADD;
+		marker_msg[i].color.r = (i == 0 ? 1.0f:0.0f);
+		marker_msg[i].color.g = (i == 1 ? 1.0f:0.0f);
+		marker_msg[i].color.b = (i == 2 ? 1.0f:0.0f);
+		marker_msg[i].color.a = 0.5;
+		marker_msg[i].lifetime = ros::Duration();
+	}
+	
+	// get table point cloud
+	tableDetector.process_cloud(transformedPoints, &filtered_msg, marker_msg);
+	
+	if (n.ok()) 
+	{
+		filteredPoints_pub.publish(filtered_msg);
+		for (i = 0; i < 3; i++) cylMarker_pub.publish(marker_msg[i]);
+		//cylMarker_pub.publish(marker_msg[0]);
+		//cylMarker_pub.publish(marker_msg[2]);
+		//cylMarker_pub.publish(marker_msg[1]);
+	}
+
+}
 
 int main(int argc, char **argv)
 {
@@ -86,3 +108,5 @@ int main(int argc, char **argv)
 	ros::spin();
   return 0;
 }
+
+
