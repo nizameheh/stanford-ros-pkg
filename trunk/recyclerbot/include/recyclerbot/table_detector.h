b440634b04dee0895a495b951cbbc53b86951483
sjh.hkust <sjh.hkust@a5c9a0f3-a0d0-8808-10fc-6cde0bab7d8d>
2010-11-05 01:28:14 +0000

    detect three bottles

--- trunk/recyclerbot/include/recyclerbot/table_detector.h
+++ trunk/recyclerbot/include/recyclerbot/table_detector.h
@@ -5,9 +5,11 @@
 #include "sensor_msgs/PointCloud.h"
 #include "sensor_msgs/ChannelFloat32.h"
 #include "geometry_msgs/Point32.h"
+#include "visualization_msgs/Marker.h"
 #include "recyclerbot/STANN/sfcnn.hpp"
 #include "recyclerbot/STANN/dpoint.hpp"
 #include "recyclerbot/STANN/rand.hpp"
+#include "math.h"
 
 using namespace std;
 
@@ -18,7 +20,9 @@ class TableDetector
 public:
   TableDetector();
   ~TableDetector(){};
-	void process_cloud(sensor_msgs::PointCloud& original_msg, sensor_msgs::PointCloud* filtered_msg);
+	void process_cloud(sensor_msgs::PointCloud& original_msg,
+	                   sensor_msgs::PointCloud* filtered_msg,
+	                   visualization_msgs::Marker* marker_msg);
 	void find_cluster(vector<geometry_msgs::Point32>& pointCloud, 
 										int k, 
 										vector<long unsigned int>& clusterId)
@@ -58,6 +62,28 @@ public:
 			}
 		}
 		//for (i = 0; i < 20; i++) cout<<answer[i]<<"; ";
+	}
+	
+	void find_cylinder(vector<geometry_msgs::Point32>& pointCloud,
+									   geometry_msgs::Point32& center,
+									   double& r,
+										 vector<long unsigned int>& clusterId);
+										 
+	void find_circle(geometry_msgs::Point32& p1, geometry_msgs::Point32& p2, geometry_msgs::Point32& p3,
+									 geometry_msgs::Point32& center, double& r)
+	{
+		double delta = (p3.x - p2.x) * (p2.y - p1.y) - (p2.x - p1.x) * (p3.y - p2.y);
+		double ss1 = p1.x * p1.x + p1.y * p1.y;
+		double ss2 = p2.x * p2.x + p2.y * p2.y;
+		double ss3 = p3.x * p3.x + p3.y * p3.y;
+		center.x = ((p3.y - p2.y) * ss1 + (p1.y - p3.y) * ss2 + (p2.y - p1.y) * ss3) / (2 * delta);
+		center.y = ((p3.x - p2.x) * ss1 + (p1.x - p3.x) * ss2 + (p2.x - p1.x) * ss3) / (2 * delta);
+		r = sqrt((center.x - p1.x)*(center.x - p1.x) + (center.y - p1.y)*(center.y - p1.y));
+	}
+	
+	double distance(geometry_msgs::Point32& p1, geometry_msgs::Point32& p2)
+	{
+		return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
 	}
 };
 
