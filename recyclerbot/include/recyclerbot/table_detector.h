d6442b5a2d605066e2f68c0a1da704ebdfa348cc
sjh.hkust <sjh.hkust@a5c9a0f3-a0d0-8808-10fc-6cde0bab7d8d>
2010-11-16 03:56:02 +0000

    ANN clustering added

--- recyclerbot/include/recyclerbot/table_detector.h
+++ recyclerbot/include/recyclerbot/table_detector.h
@@ -12,7 +12,7 @@
 #include "math.h"
 
 #define MAX_CYL_NUM 10
-#define CYL_POINT_THRESH 2000
+#define CYL_POINT_THRESH 3500
 #define COLORNUM 10
 
 float color_array[COLORNUM][4] = {
@@ -56,46 +56,10 @@ public:
 	                   vector<visualization_msgs::Marker>& marker_msg);
 	void find_cluster(vector<geometry_msgs::Point32>& pointCloud, 
 										int k, 
-										vector<long unsigned int>& clusterId)
-	{
-		int i = 0;
-		vector<Point> points;
-		Point p_;
-		vector<double> distance;
-		int size = pointCloud.size();
-		int iter = 10;
-		int startPoint = 0;
-		
-		for (i = 0; i < size; i++)
-		{
-			p_[0] = pointCloud[i].x;
-			p_[1] = pointCloud[i].y;
-			p_[2] = pointCloud[i].z;
-			points.push_back(p_);
-		}
-		
-		sfcnn<Point, 3, double>
-		  NN(&points[0], size);
-
-		int minDistance = 1000;
-//		int minDisIter = 0;
-		vector<long unsigned int> tempCluster;
-		
-		for (i = 0; i < iter; i++)
-		{
-			startPoint = int(__drand48__() * (size - 1));
-			NN.ksearch(points[startPoint], k, tempCluster, distance);
-			if (distance.back() < minDistance)
-			{
-				minDistance = distance.back();
-				clusterId = tempCluster;
-				tempCluster.clear();
-			}
-		}
-		//for (i = 0; i < 20; i++) cout<<answer[i]<<"; ";
-	}
+										vector<long unsigned int>& clusterId);
 	
 	void find_cylinder(vector<geometry_msgs::Point32>& pointCloud,
+										 vector<long unsigned int>& preclusterId,
 									   cylinder& cylinder_,
 										 vector<long unsigned int>& clusterId);
 										 
